17c982fb0f936fbf5f2acf3798f072dbcba8cfb5
Chirayu Desai <chirayudesai1@gmail.com>
2016-08-26 22:13:51 +0530

    envsetup: fix lunch
    Change-Id: I9dea4605857741132b8d10143c20d4bd7829086a

--- envsetup.sh
+++ envsetup.sh
@@ -596,9 +596,11 @@ function lunch()
     fi
 
     local product=$(echo -n $selection | sed -e "s/-.*$//")
+    check_product $product
     TARGET_PRODUCT=$product \
     TARGET_BUILD_VARIANT=$variant \
     build_build_var_cache
+
     if [ $? -ne 0 ]
     then
         echo
