2163a26d41b80a052323bd69e6dea5af42b2e227
Jacob McSwain <jacob.a.mcswain@gmail.com>
2017-08-06 12:53:25 -0500

    Don't add debug info to dex preopt

--- core/dex_preopt.mk
+++ core/dex_preopt.mk
@@ -34,7 +34,7 @@ SYSTEM_OTHER_ODEX_FILTER ?= app/% priv-app/%
 #    WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
 #  endif
 # Add mini-debug-info to the boot classpath unless explicitly asked not to.
-#  ifneq (false,$(WITH_DEXPREOPT_DEBUG_INFO))
+#  ifeq (true,$(WITH_DEXPREOPT_DEBUG_INFO))
 #    PRODUCT_DEX_PREOPT_BOOT_FLAGS += --generate-mini-debug-info
 #  endif
 #endif
