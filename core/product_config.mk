29cf4608ad9669bb11ae13d7b47e93833ad2c2fd
liquid0624 <rjbirnesser@gmail.com>
2017-09-10 02:21:48 -0400

    build: initial deso make changes

--- core/product_config.mk
+++ core/product_config.mk
@@ -179,9 +179,9 @@ include $(BUILD_SYSTEM)/node_fns.mk
 include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
-# A Lineage build needs only the Lineage product makefiles.
-ifneq ($(LINEAGE_BUILD),)
-  all_product_configs := $(shell find device -path "*/$(LINEAGE_BUILD)/lineage.mk")
+# A Desolation build needs only the Desolation product makefiles.
+ifneq ($(DESO_BUILD),)
+  all_product_configs := $(shell find device -path "*/$(DESO_BUILD)/deso.mk")
 else
   ifneq ($(strip $(TARGET_BUILD_APPS)),)
   # An unbundled app build needs only the core product makefiles.
@@ -192,9 +192,9 @@ else
   # files in the tree.
   all_product_configs := $(get-all-product-makefiles)
   endif # TARGET_BUILD_APPS
-endif # LINEAGE_BUILD
+endif # DESO_BUILD
 
-ifeq ($(LINEAGE_BUILD),)
+ifeq ($(DESO_BUILD),)
 all_named_products :=
 
 # Find the product config makefile for the current product.
